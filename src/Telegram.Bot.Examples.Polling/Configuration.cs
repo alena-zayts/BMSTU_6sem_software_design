9e60e819799c2a973e0fae50d2e4a715a17fbd3e
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2022-03-28 23:16:38 +0300

    updates for readmes

---
+++ src/Telegram.Bot.Examples.Polling/Configuration.cs
@@ -0,0 +1,8 @@
+
+namespace Telegram.Bot.Examples.Polling
+{
+    public static class Configuration
+    {
+        public readonly static string BotToken = "5116348352:AAGc2737xTkAOiKg8oXuQeLAbKq5e0IdIVg";
+    }
+}
