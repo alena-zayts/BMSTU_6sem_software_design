d1dfe0ec3c7931d8bc94726e30817ae4383373fc
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2022-05-03 16:33:40 +0300

    first success in ui

---
+++ src/SkiResort/AuthorizationComponent/AuthorizationForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace AuthorizationForm
+{
+    partial class AuthorizationForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logInButton = new System.Windows.Forms.Button();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.continueUnauthorizedButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.cardIDTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // logInButton
+            // 
+            this.logInButton.Location = new System.Drawing.Point(144, 209);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(139, 48);
+            this.logInButton.TabIndex = 0;
+            this.logInButton.Text = "Войти";
+            this.logInButton.UseVisualStyleBackColor = true;
+            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(335, 209);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(229, 48);
+            this.registerButton.TabIndex = 1;
+            this.registerButton.Text = "Зарегистрироваться";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // continueUnauthorizedButton
+            // 
+            this.continueUnauthorizedButton.Location = new System.Drawing.Point(91, 293);
+            this.continueUnauthorizedButton.Name = "continueUnauthorizedButton";
+            this.continueUnauthorizedButton.Size = new System.Drawing.Size(525, 48);
+            this.continueUnauthorizedButton.TabIndex = 2;
+            this.continueUnauthorizedButton.Text = "Продолжить как неавторизованный пользователь";
+            this.continueUnauthorizedButton.UseVisualStyleBackColor = true;
+            this.continueUnauthorizedButton.Click += new System.EventHandler(this.continueUnauthorizedButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Логин (email):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(410, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Номер проездной карты (необязательно):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Пароль:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(291, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 7;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(441, 59);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(200, 29);
+            this.emailTextBox.TabIndex = 8;
+            // 
+            // cardIDTextBox
+            // 
+            this.cardIDTextBox.Location = new System.Drawing.Point(441, 139);
+            this.cardIDTextBox.Name = "cardIDTextBox";
+            this.cardIDTextBox.Size = new System.Drawing.Size(200, 29);
+            this.cardIDTextBox.TabIndex = 9;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(441, 99);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(200, 29);
+            this.passwordTextBox.TabIndex = 10;
+            // 
+            // AuthorizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 364);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.cardIDTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.continueUnauthorizedButton);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.logInButton);
+            this.Name = "AuthorizationForm";
+            this.Text = "Вход";
+            this.Load += new System.EventHandler(this.AuthorizationForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button logInButton;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button continueUnauthorizedButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox cardIDTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+    }
+}
+
---
+++ src/SkiResort/AuthorizationComponent/AuthorizationForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BL.Models;
+
+namespace AuthorizationForm
+{
+    public partial class AuthorizationForm : Form
+    {
+        //private 
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthorizationForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logInButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void continueUnauthorizedButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
