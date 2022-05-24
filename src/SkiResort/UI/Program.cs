d6d4079db4409bcf2331a5532b53b6136943e093
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2022-05-24 09:50:46 +0300

    wow

--- src/SkiResort/UI/Program.cs
+++ src/SkiResort/UI/Program.cs
@@ -38,25 +38,17 @@ namespace UI
             IViewsFactory viewsFactory = new WinFormViewsFactory();
             Facade facade = new(repositoryFactory);
             Presenter presenter = new(viewsFactory, facade);
-            presenter.RunAsync();
+            //presenter.RunAsync();
 
-            //Task.Run(() => presenter.RunAsync());
+            Task.Run(() => presenter.RunAsync());
 
 
-            //IHost host = Host.CreateDefaultBuilder().ConfigureServices(services =>
-            //{
-            //    services.AddHostedService<QueueTimeCountingService>();
-            //    services.AddHostedService<CardReadingReceivingService>();
-            //}).Build();
-            //host.RunAsync();
+            IHost host = Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+                services.AddHostedService<QueueTimeCountingService>();
+                services.AddHostedService<CardReadingReceivingService>();
+            }).Build();
+            host.RunAsync();
         }
     }
-    //public static class DiExtensions
-    //{
-    //    public static void AddRepositoryExtensions(IServiceCollection services)
-    //    {
-    //        services.AddSingleton<IRepositoriesFactory, TarantoolRepositoriesFactory>();
-    //        services.AddSingleton<IViewsFactory, WinFormViewsFactory>();
-    //    }
-    //}
 }
